c675e74f85c360a660aa8b49976850d9a425722d
Shahadat Hossain <mshossain509@gmail.com>
2022-09-14 01:19:02 +0600

    added register page

--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,11 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
+      path: '/register',
+      name: 'Register',
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
       path: '/profile',
       name: 'Profile',
       component: () => import('../views/ProfileView.vue')
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import RegisterFrom from "@/components/register/RegisterForm.vue";
+</script>
+
+<template>
+  <RegisterFrom />
+</template>
