519e86d975f704c2985382896acd29a54f36c23d
Shahadat Hossain <mshossain509@gmail.com>
2022-09-14 01:12:05 +0600

    re-suffle component of account page

--- src/components/account/AccountWrapper.vue
+++ src/components/account/AccountWrapper.vue
@@ -1,6 +1,6 @@
 <script setup>
-import AccountSidebar from '@/components/account/accountwrapperComponent/AccountSidebar.vue';
-import AccountInfo from '@/components/account/accountwrapperComponent/AccountInfo.vue';
+import AccountSidebar from '@/components/common/AccountSidebar.vue';
+import AccountInfo from '@/components/account/AccountInfo.vue';
 </script>
 <template>
   <div class="container grid grid-cols-12 items-start gap-6 pt-4 pb-16">
