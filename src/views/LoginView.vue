5d19537392d76e9642cad0d06aeaec2387507cac
Shahadat Hossain <mshossain509@gmail.com>
2022-09-14 01:19:12 +0600

    update login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,7 @@
 <script setup>
-import TheBreadcrumb from "@/components/common/TheBreadcrumb.vue";
 import LoginFrom from "@/components/login/LoginForm.vue";
 </script>
 
 <template>
-  <TheBreadcrumb />
   <LoginFrom />
 </template>
