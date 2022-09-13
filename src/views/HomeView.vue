44fc67f6ffc3cabe39790b0ae21d2c7d293291ba
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2022-09-13 14:21:17 +0600

    removed unwanted files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </main>
 </template>
