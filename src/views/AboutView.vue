01ab66f6b78645fc32bbe72d70db5e2dc288664d
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2022-09-13 15:47:39 +0600

    added scss to project and tested the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="about">
-    <h1 className="text-2xl font-bold underline text-indigo-100 about-hello">About</h1>
+    <h1 className="text-2xl font-bold underline text-indigo-100 about-hello">
+      About
+    </h1>
   </div>
 </template>
-<style scoped>
-.about-hello {
-  @apply bg-red-500 !important;
+<style lang="scss" scoped>
+.about {
+  .about-hello {
+    @apply bg-red-500 #{!important};
+  }
 }
 </style>
