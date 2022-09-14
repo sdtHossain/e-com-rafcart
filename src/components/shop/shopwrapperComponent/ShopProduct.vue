1bce16912667927aeb6c4016d1a59e8542ade5ce
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2022-09-14 17:15:08 +0600

    implement hook to shop page

--- src/components/shop/shopwrapperComponent/ShopProduct.vue
+++ src/components/shop/shopwrapperComponent/ShopProduct.vue
@@ -4,10 +4,7 @@ import { RouterLink } from "vue-router";
 
 import { getData } from '../../../hook/getDataSet'
 
-const products = ref()
-onMounted(() => {
-  products.value = getData('products')
-})
+const products = getData('products')
 </script>
 <template>
   <div class="col-span-3">
@@ -38,11 +35,11 @@ onMounted(() => {
     </div>
 
     <div class="grid grid-cols-3 gap-6">
-      <div class="bg-white shadow rounded overflow-hidden group">
+      <div v-for="product in products" :key="product.title" class="bg-white shadow rounded overflow-hidden group">
         <div class="relative">
           <img
-            src="../../../assets/images/products/product1.jpg"
-            alt="product 1"
+            :src="product.thumbnails"
+            :alt="product.title"
             class="w-full"
           />
           <div
@@ -69,11 +66,11 @@ onMounted(() => {
             <h4
               class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
             >
-              Guyer Chair
+              {{ product.title }}
             </h4>
           </router-link>
           <div class="flex items-baseline mb-1 space-x-2">
-            <p class="text-xl text-primary font-semibold">$45.00</p>
+            <p class="text-xl text-primary font-semibold">{{ product.price }}</p>
             <p class="text-sm text-gray-400 line-through">$55.90</p>
           </div>
           <div class="flex items-center">
@@ -84,287 +81,7 @@ onMounted(() => {
               <span><i class="fa-solid fa-star"></i></span>
               <span><i class="fa-solid fa-star"></i></span>
             </div>
-            <div class="text-xs text-gray-500 ml-3">(150)</div>
-          </div>
-        </div>
-        <a
-          href="#"
-          class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
-          >Add to cart</a
-        >
-      </div>
-
-      <div class="bg-white shadow rounded overflow-hidden group">
-        <div class="relative">
-          <img
-            src="../../../assets/images/products/product2.jpg"
-            alt="product 1"
-            class="w-full"
-          />
-          <div
-            class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition"
-          >
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="view product"
-            >
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </a>
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="add to wishlist"
-            >
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="pt-4 pb-3 px-4">
-          <a href="#">
-            <h4
-              class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-            >
-              Guyer Chair
-            </h4>
-          </a>
-          <div class="flex items-baseline mb-1 space-x-2">
-            <p class="text-xl text-primary font-semibold">$45.00</p>
-            <p class="text-sm text-gray-400 line-through">$55.90</p>
-          </div>
-          <div class="flex items-center">
-            <div class="flex gap-1 text-sm text-yellow-400">
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-            </div>
-            <div class="text-xs text-gray-500 ml-3">(150)</div>
-          </div>
-        </div>
-        <a
-          href="#"
-          class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
-          >Add to cart</a
-        >
-      </div>
-
-      <div class="bg-white shadow rounded overflow-hidden group">
-        <div class="relative">
-          <img
-            src="../../../assets/images/products/product3.jpg"
-            alt="product 1"
-            class="w-full"
-          />
-          <div
-            class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition"
-          >
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="view product"
-            >
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </a>
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="add to wishlist"
-            >
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="pt-4 pb-3 px-4">
-          <a href="#">
-            <h4
-              class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-            >
-              Guyer Chair
-            </h4>
-          </a>
-          <div class="flex items-baseline mb-1 space-x-2">
-            <p class="text-xl text-primary font-semibold">$45.00</p>
-            <p class="text-sm text-gray-400 line-through">$55.90</p>
-          </div>
-          <div class="flex items-center">
-            <div class="flex gap-1 text-sm text-yellow-400">
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-            </div>
-            <div class="text-xs text-gray-500 ml-3">(150)</div>
-          </div>
-        </div>
-        <a
-          href="#"
-          class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
-          >Add to cart</a
-        >
-      </div>
-
-      <div class="bg-white shadow rounded overflow-hidden group">
-        <div class="relative">
-          <img
-            src="../../../assets/images/products/product4.jpg"
-            alt="product 1"
-            class="w-full"
-          />
-          <div
-            class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition"
-          >
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="view product"
-            >
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </a>
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="add to wishlist"
-            >
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="pt-4 pb-3 px-4">
-          <a href="#">
-            <h4
-              class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-            >
-              Guyer Chair
-            </h4>
-          </a>
-          <div class="flex items-baseline mb-1 space-x-2">
-            <p class="text-xl text-primary font-semibold">$45.00</p>
-            <p class="text-sm text-gray-400 line-through">$55.90</p>
-          </div>
-          <div class="flex items-center">
-            <div class="flex gap-1 text-sm text-yellow-400">
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-            </div>
-            <div class="text-xs text-gray-500 ml-3">(150)</div>
-          </div>
-        </div>
-        <a
-          href="#"
-          class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
-          >Add to cart</a
-        >
-      </div>
-
-      <div class="bg-white shadow rounded overflow-hidden group">
-        <div class="relative">
-          <img
-            src="../../../assets/images/products/product5.jpg"
-            alt="product 1"
-            class="w-full"
-          />
-          <div
-            class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition"
-          >
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="view product"
-            >
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </a>
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="add to wishlist"
-            >
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="pt-4 pb-3 px-4">
-          <a href="#">
-            <h4
-              class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-            >
-              Guyer Chair
-            </h4>
-          </a>
-          <div class="flex items-baseline mb-1 space-x-2">
-            <p class="text-xl text-primary font-semibold">$45.00</p>
-            <p class="text-sm text-gray-400 line-through">$55.90</p>
-          </div>
-          <div class="flex items-center">
-            <div class="flex gap-1 text-sm text-yellow-400">
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-            </div>
-            <div class="text-xs text-gray-500 ml-3">(150)</div>
-          </div>
-        </div>
-        <a
-          href="#"
-          class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
-          >Add to cart</a
-        >
-      </div>
-
-      <div class="bg-white shadow rounded overflow-hidden group">
-        <div class="relative">
-          <img
-            src="../../../assets/images/products/product6.jpg"
-            alt="product 1"
-            class="w-full"
-          />
-          <div
-            class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition"
-          >
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="view product"
-            >
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </a>
-            <a
-              href="#"
-              class="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-              title="add to wishlist"
-            >
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="pt-4 pb-3 px-4">
-          <a href="#">
-            <h4
-              class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-            >
-              Guyer Chair
-            </h4>
-          </a>
-          <div class="flex items-baseline mb-1 space-x-2">
-            <p class="text-xl text-primary font-semibold">$45.00</p>
-            <p class="text-sm text-gray-400 line-through">$55.90</p>
-          </div>
-          <div class="flex items-center">
-            <div class="flex gap-1 text-sm text-yellow-400">
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-              <span><i class="fa-solid fa-star"></i></span>
-            </div>
-            <div class="text-xs text-gray-500 ml-3">(150)</div>
+            <div class="text-xs text-gray-500 ml-3">({{ product.stock }})</div>
           </div>
         </div>
         <a
--- src/hook/getDataSet.js
+++ src/hook/getDataSet.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, reactive, toRef } from 'vue'
 
 
 export function getData(dataSet) {
@@ -6,6 +6,8 @@ export function getData(dataSet) {
   fetch(`https://dummyjson.com/${dataSet}`)
   .then(res => res.json())
   .then(data => resData.value = data[dataSet])
+
+  console.log("from hook ", resData.value)
 
   return resData
 }
