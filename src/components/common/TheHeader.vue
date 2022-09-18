1c9f8f722409d69f39ea40819d38cedeaf2b6383
Shahadat Hossain <mshossain509@gmail.com>
2022-09-19 01:33:15 +0600

    added pinia and implent this to manage cart functionality

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import { useCartStore } from '../../store/cart'
+
+const { cartItems } = useCartStore()
 </script>
 
 <template>
@@ -38,7 +41,7 @@ import { RouterLink } from "vue-router";
           <div
             class="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs"
           >
-            8
+            {{ cartItems.length }}
           </div>
         </router-link>
         <a
--- src/components/wishlist/WishlistItems.vue
+++ src/components/wishlist/WishlistItems.vue
@@ -1,87 +1,36 @@
+<script setup>
+import { useCartStore } from "../../store/cart";
+
+const { cartItems } = useCartStore()
+</script>
+
 <template>
   <div class="col-span-9 space-y-4">
     <div
+      v-for="product in cartItems"
+      :key="product.id"
       class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded"
     >
       <div class="w-28">
         <img
-          src="../../assets/images/products/product6.jpg"
+          :src="product.thumbnail"
           alt="product 6"
           class="w-full"
         />
       </div>
       <div class="w-1/3">
         <h2 class="text-gray-800 text-xl font-medium uppercase">
-          Italian L shape
+          {{ product.title }}
         </h2>
         <p class="text-gray-500 text-sm">
           Availability: <span class="text-green-600">In Stock</span>
         </p>
       </div>
-      <div class="text-primary text-lg font-semibold">$320.00</div>
-      <a
-        href="#"
-        class="px-6 py-2 text-center text-sm text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium"
-        >add to cart</a
-      >
+      <div class="text-primary text-lg font-semibold">${{ product.price }}</div>
 
-      <div class="text-gray-600 cursor-pointer hover:text-primary">
-        <i class="fa-solid fa-trash"></i>
-      </div>
-    </div>
+      <div class="text-primary text-lg font-semibold">{{ product.quantity }}pcs</div>
 
-    <div
-      class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded"
-    >
-      <div class="w-28">
-        <img
-          src="../../assets/images/products/product5.jpg"
-          alt="product 6"
-          class="w-full"
-        />
-      </div>
-      <div class="w-1/3">
-        <h2 class="text-gray-800 text-xl font-medium uppercase">
-          Dining Table
-        </h2>
-        <p class="text-gray-500 text-sm">
-          Availability: <span class="text-green-600">In Stock</span>
-        </p>
-      </div>
-      <div class="text-primary text-lg font-semibold">$320.00</div>
-      <a
-        href="#"
-        class="px-6 py-2 text-center text-sm text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium"
-        >add to cart</a
-      >
-
-      <div class="text-gray-600 cursor-pointer hover:text-primary">
-        <i class="fa-solid fa-trash"></i>
-      </div>
-    </div>
-
-    <div
-      class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded"
-    >
-      <div class="w-28">
-        <img
-          src="../../assets/images/products/product10.jpg"
-          alt="product 6"
-          class="w-full"
-        />
-      </div>
-      <div class="w-1/3">
-        <h2 class="text-gray-800 text-xl font-medium uppercase">Sofa</h2>
-        <p class="text-gray-500 text-sm">
-          Availability: <span class="text-red-600">Out of Stock</span>
-        </p>
-      </div>
-      <div class="text-primary text-lg font-semibold">$320.00</div>
-      <a
-        href="#"
-        class="cursor-not-allowed px-6 py-2 text-center text-sm text-white bg-red-400 border border-red-400 rounded transition uppercase font-roboto font-medium"
-        >add to cart</a
-      >
+      <div class="text-primary text-lg font-semibold">${{ product.quantity * product.price }}</div>
 
       <div class="text-gray-600 cursor-pointer hover:text-primary">
         <i class="fa-solid fa-trash"></i>
