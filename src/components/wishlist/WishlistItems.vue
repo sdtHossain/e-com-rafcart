b4b0af96ddfbbb24f441157b1ee10b9fd8712595
Shahadat Hossain <mshossain509@gmail.com>
2022-09-21 03:30:33 +0600

    fixed pinia storeToRefs issue by destructuring from store object

--- src/components/wishlist/WishlistItems.vue
+++ src/components/wishlist/WishlistItems.vue
@@ -1,27 +1,31 @@
 <script setup>
-  import { ref } from 'vue'
+import { ref, unref } from "vue";
 import { useCartStore } from "../../store/cart";
+import { storeToRefs } from "pinia";
 
-const { cartItems } = useCartStore()
+// const cartStore = useCartStore();
+const { cartItems } = storeToRefs(useCartStore());
 const total = ref(0);
-cartItems.forEach((product) => {
+cartItems.value.forEach((product) => {
   total.value += product.price;
 });
+
+console.log(cartItems)
+// console.log(ref(cartItems))
+
+//deleteCartItem
+const { deleteCartItem } = useCartStore();
 </script>
 
 <template>
-  <div class="col-span-9 space-y-4">
+  <div v-if="cartItems" class="col-span-9 space-y-4">
     <div
       v-for="product in cartItems"
       :key="product.id"
       class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded"
     >
       <div class="w-28">
-        <img
-          :src="product.thumbnail"
-          alt="product 6"
-          class="w-full"
-        />
+        <img :src="product.thumbnail" alt="product 6" class="w-full" />
       </div>
       <div class="w-1/3">
         <h2 class="text-gray-800 text-xl font-medium uppercase">
@@ -33,11 +37,18 @@ cartItems.forEach((product) => {
       </div>
       <div class="text-primary text-lg font-semibold">${{ product.price }}</div>
 
-      <div class="text-primary text-lg font-semibold">{{ product.quantity }}pcs</div>
+      <div class="text-primary text-lg font-semibold">
+        {{ product.quantity }}pcs
+      </div>
 
-      <div class="text-primary text-lg font-semibold">${{ product.quantity * product.price }}</div>
+      <div class="text-primary text-lg font-semibold">
+        ${{ product.quantity * product.price }}
+      </div>
 
-      <div class="text-gray-600 cursor-pointer hover:text-primary">
+      <div
+        @click="deleteCartItem"
+        class="text-gray-600 cursor-pointer hover:text-primary"
+      >
         <i class="fa-solid fa-trash"></i>
       </div>
     </div>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,13 +1,30 @@
+import { ref } from 'vue'
 import { defineStore } from "pinia";
 
-export const useCartStore = defineStore("cart", {
-  state: () => ({
-    cartItems: [],
-  }),
-  actions: {
-    addProductToCart(product) {
-      product.quantity = 1
-      this.cartItems.push(product)
-    }
+// export const useCartStore = defineStore("cart", {
+//   state: () => ({
+//     cartItems: [],
+//   }),
+//   actions: {
+//     addProductToCart(product) {
+//       product.quantity = 1
+//       this.cartItems.push(product)
+//     }
+//   }
+// });
+
+
+
+export const useCartStore = defineStore('cart', () => {
+  const cartItems = ref([])
+  const addProductToCart = (product) => {
+    product.quantity = 1
+    cartItems.value.push(product)
   }
-});
+
+  const deleteCartItem = () => {
+    console.log(cartItems.value)
+  }
+
+  return { cartItems, addProductToCart, deleteCartItem }
+})
