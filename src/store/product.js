a2b110d98eace8a6403f02a4df5e73c0ba0e00b2
Shahadat Hossain <mshossain509@gmail.com>
2022-09-24 20:49:38 +0600

    added getNewArrival and getRecomended Products getter

--- src/store/product.js
+++ src/store/product.js
@@ -1,25 +1,42 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useProductStore = defineStore('product', () => {
-  const products = ref([])
-  const categories = ref()
-
-  
+export const useProductStore = defineStore("product", () => {
+  const products = ref([]);
+  const categories = ref();
 
   async function fetchProducts() {
-    let res = await fetch('https://dummyjson.com/products')
-    let data = await res.json()
-    products.value = await data.products
+    let res = await fetch("https://dummyjson.com/products");
+    let data = await res.json();
+    products.value = await data.products;
   }
 
   const getProductCategories = computed(() => {
-    return [...new Set(products.value.map(product => product.category))]
-  })
+    return [...new Set(products.value.map((product) => product.category))];
+  });
 
   const getProductByCategory = computed(() => {
-    return (category) => products.value.filter(product => product.category === category)
-  })
+    return (category) =>
+      products.value.filter((product) => product.category === category);
+  });
 
-  return { products, getProductCategories, fetchProducts, getProductByCategory }
-})
+  const getNewArrival = computed(() => {
+    let length = products.value.length;
+    return products.value.filter((product) => product.id > length - 4);
+  });
+
+  const getRecomendedProducts = computed(() => {
+    return products.value.filter(
+      (product) => product.id > 20 && product.id < 29
+    );
+  });
+
+  return {
+    products,
+    getProductCategories,
+    fetchProducts,
+    getProductByCategory,
+    getNewArrival,
+    getRecomendedProducts,
+  };
+});
