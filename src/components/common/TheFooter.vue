2e7ee459deea8a1f6c3c057d890ba12da6dfb98d
Shahadat Hossain <mshossain509@gmail.com>
2022-10-01 04:15:19 +0600

    update footer copyright

--- src/components/common/TheFooter.vue
+++ src/components/common/TheFooter.vue
@@ -156,7 +156,9 @@
   <!-- copyright -->
   <div class="bg-gray-800 py-4">
     <div class="container flex items-center justify-between">
-      <p class="text-white">&copy; TailCommerce - All Right Reserved</p>
+      <p class="text-white">
+        &copy; TailCommerce - All Right Reserved | vue developed by Shahadat
+      </p>
       <div>
         <img src="../../assets/images/methods.png" alt="methods" class="h-5" />
       </div>
