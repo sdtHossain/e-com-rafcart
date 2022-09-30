8310d716715e826ad36d8f038e0af95cc7fd0cfe
Shahadat Hossain <mshossain509@gmail.com>
2022-10-01 00:33:54 +0600

    fixed product design

--- src/components/common/ProductCard.vue
+++ src/components/common/ProductCard.vue
@@ -11,8 +11,12 @@ const props = defineProps(["products"]);
     :key="product.title"
     class="bg-white shadow rounded overflow-hidden group"
   >
-    <div class="relative">
-      <img :src="product.thumbnail" :alt="product.title" class="w-full" />
+    <div class="relative img-wrap">
+      <img
+        :src="product.thumbnail"
+        :alt="product.title"
+        class="w-full h-full object-cover"
+      />
       <div
         class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition"
       >
@@ -32,36 +36,49 @@ const props = defineProps(["products"]);
         </a>
       </div>
     </div>
-    <div class="pt-4 pb-3 px-4">
-      <router-link to="/product/1" href="#">
-        <h4
-          class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-        >
-          {{ product.title }}
-        </h4>
-      </router-link>
-      <div class="flex items-baseline mb-1 space-x-2">
-        <p class="text-xl text-primary font-semibold">
-          {{ product.price }}
-        </p>
-        <p class="text-sm text-gray-400 line-through">$55.90</p>
-      </div>
-      <div class="flex items-center">
-        <div class="flex gap-1 text-sm text-yellow-400">
-          <span><i class="fa-solid fa-star"></i></span>
-          <span><i class="fa-solid fa-star"></i></span>
-          <span><i class="fa-solid fa-star"></i></span>
-          <span><i class="fa-solid fa-star"></i></span>
-          <span><i class="fa-solid fa-star"></i></span>
+    <div class="relative bottom-content">
+      <div class="pt-4 pb-3 px-4 info-box flex flex-col">
+        <router-link to="/product/1" href="#">
+          <h4
+            class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
+          >
+            {{ product.title }}
+          </h4>
+        </router-link>
+        <div class="flex items-baseline mb-1 space-x-2 mt-auto">
+          <p class="text-xl text-primary font-semibold">
+            {{ product.price }}
+          </p>
+          <p class="text-sm text-gray-400 line-through">$55.90</p>
         </div>
-        <div class="text-xs text-gray-500 ml-3">({{ product.stock }})</div>
+        <div class="flex items-center">
+          <div class="flex gap-1 text-sm text-yellow-400">
+            <span><i class="fa-solid fa-star"></i></span>
+            <span><i class="fa-solid fa-star"></i></span>
+            <span><i class="fa-solid fa-star"></i></span>
+            <span><i class="fa-solid fa-star"></i></span>
+            <span><i class="fa-solid fa-star"></i></span>
+          </div>
+          <div class="text-xs text-gray-500 ml-3">({{ product.stock }})</div>
+        </div>
       </div>
+      <button
+        @click="addProductToCart(product)"
+        class="absolute bottom-0 block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
+      >
+        Add to cart
+      </button>
     </div>
-    <button
-      @click="addProductToCart(product)"
-      class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
-    >
-      Add to cart
-    </button>
   </div>
 </template>
+<style scoped>
+.img-wrap {
+  height: 160px;
+}
+.bottom-content {
+  height: 180px;
+}
+.info-box {
+  height: calc(100% - 34px);
+}
+</style>
