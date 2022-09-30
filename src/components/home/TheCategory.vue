710989088a1a9723fd8ba8e87e741bdeb9ca0283
Shahadat Hossain <mshossain509@gmail.com>
2022-10-01 04:15:31 +0600

    update category image path

--- src/components/home/TheCategory.vue
+++ src/components/home/TheCategory.vue
@@ -5,10 +5,11 @@ import { storeToRefs } from "pinia";
 const { products, getProductCategories } = storeToRefs(useProductStore());
 const { fetchProducts } = useProductStore();
 
-fetchProducts()
+fetchProducts();
 
-
-
+const getImage = (img) => {
+  return `src/assets/images/category/category-${img}.jpg`;
+};
 </script>
 
 <template>
@@ -17,15 +18,16 @@ fetchProducts()
       shop by category
     </h2>
     <div class="grid grid-cols-3 gap-3">
-      <div v-for="(category, index) in getProductCategories" :key="index" class="relative rounded-sm overflow-hidden group">
-        <img
-          :src="`src/assets/images/category/category-${index + 1}.jpg`"
-          alt="{{category}}"
-          class="w-full"
-        />
-        <router-link :to="`/category/${category}`"
+      <div
+        v-for="(category, index) in getProductCategories"
+        :key="index"
+        class="relative rounded-sm overflow-hidden group"
+      >
+        <img :src="getImage(index + 1)" alt="{{category}}" class="w-full" />
+        <router-link
+          :to="`/category/${category}`"
           class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >{{category.toUpperCase()}}</router-link
+          >{{ category.toUpperCase() }}</router-link
         >
       </div>
     </div>
