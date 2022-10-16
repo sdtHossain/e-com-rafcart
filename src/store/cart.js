007b888be79dfb6965a713b2f168a018460b89ad
Shahadat Hossain <mshossain509@gmail.com>
2022-10-16 21:50:24 +0600

    fixed cart item duplicate issue on cart list

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,23 +1,15 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-// export const useCartStore = defineStore("cart", {
-//   state: () => ({
-//     cartItems: [],
-//   }),
-//   actions: {
-//     addProductToCart(product) {
-//       product.quantity = 1
-//       this.cartItems.push(product)
-//     }
-//   }
-// });
-
 export const useCartStore = defineStore("cart", () => {
   const cartItems = ref([]);
   const totalAmount = ref(0);
   const productQuantity = ref(1);
   const test = ref(0);
+  const listedProduct = ref();
+  const cartTitles = computed(() => {
+    return [...new Set(cartItems.value.map((product) => product.title))];
+  });
 
   function calculateTotal() {
     totalAmount.value = 0;
@@ -27,9 +19,17 @@ export const useCartStore = defineStore("cart", () => {
   }
 
   const addProductToCart = (product, quantity = 1) => {
-    if (!cartItems.value.includes(product)) {
+    if (!cartTitles.value.includes(product.title)) {
       product.quantity = quantity;
       cartItems.value.push(product);
+    } else {
+      if (quantity > 1) {
+        listedProduct.value = cartItems.value.find(
+          (item) => item.title == product.title
+        );
+
+        listedProduct.value.quantity += quantity;
+      }
     }
     calculateTotal();
   };
