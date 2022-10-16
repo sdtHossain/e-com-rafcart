aae519a50f01db00e600b22caa0353811e4179b0
Shahadat Hossain <mshossain509@gmail.com>
2022-10-16 10:34:08 +0600

    tried to do override tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "regal-blue": "#243c5a",
+        red: colors.indigo,
+        primary: "#000000",
       },
     },
   },
